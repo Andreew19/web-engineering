87b0acd7e9124585dde6705d63dcdb2a01446b64
Andreew19 <andrew_04@list.ru>
2022-08-20 15:21:35 +0300

    add some cards

--- web-en/src/App.vue
+++ web-en/src/App.vue
@@ -45,51 +45,97 @@
       </div>
       <div class="wrapper">
         <h1 class="main-title">Добавить блок</h1>
-        <div class="components-bar">
-          <v-card
-    class="mx-auto"
-    max-width="344"
-  >
-    <v-img
-      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-      height="200px"
-    ></v-img>
+        <div class="component-bar d-flex justify-space-around mb-6 ">
+            <v-card
+          class="card-tool"
+          color="#F5F5F5"
+          max-width="344"
+          outlined
+          >
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="text-overline mb-4">
+          <v-avatar
+              class="avatar-tool"
+              color="primary"
+              size="50"
+          >
+            
+          </v-avatar>
+        </div>
+        <v-list-item-title class="text-h5 mb-1">
+          Изображение
+        </v-list-item-title>
+        
+      </v-list-item-content>
 
-    <v-card-title>
-      Top western road trips
-    </v-card-title>
+      
+    </v-list-item>
 
-    <v-card-subtitle>
-      1,000 miles of wonder
-    </v-card-subtitle>
+    
+  </v-card>
 
-    <v-card-actions>
-      <v-btn
-        color="orange lighten-2"
-        text
-      >
-        Explore
-      </v-btn>
+  <v-card
+          class="card-tool"
+          color="#F5F5F5"
+          max-width="344"
+          outlined
+          >
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="text-overline mb-4">
+          <v-avatar
+              class="avatar-tool"
+              color="primary"
+              size="50"
+          >
+            
+          </v-avatar>
+        </div>
+        <v-list-item-title class="text-h5 mb-1">
+          Текст
+        </v-list-item-title>
+        
+      </v-list-item-content>
 
-      <v-spacer></v-spacer>
+      
+    </v-list-item>
 
-      <v-btn
-        icon
-        @click="show = !show"
-      >
-        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-      </v-btn>
-    </v-card-actions>
+    
+  </v-card>
 
-    <v-expand-transition>
-      <div v-show="show">
-        <v-divider></v-divider>
+  <v-card
+          class="card-tool"
+          color="#F5F5F5"
+          max-width="344"
+          outlined
+          >
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="text-overline mb-4">
+          <v-avatar
+              class="avatar-tool"
+              color="primary"
+              size="50"
+          >
+            
+          </v-avatar>
+        </div>
+        <v-list-item-title class="text-h5 mb-1">
+          Шаблоны
+        </v-list-item-title>
+        
+      </v-list-item-content>
 
-        <v-card-text>
-          I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
-        </v-card-text>
-      </div>
-    </v-expand-transition>
+      
+
+  
+
+  
+  
+    </v-list-item>
+
+    
   </v-card>
         </div>
       </div>
@@ -159,6 +205,16 @@ main{
 }
 .main-title{
   margin-top: 126px;
+}
+
+.card-tool{
+  width: 250px;
+  height: 125px;
+  border-radius: 30px;
+}
+
+.component-bar{
+  margin-top: 52px;
 }
 
 </style>
